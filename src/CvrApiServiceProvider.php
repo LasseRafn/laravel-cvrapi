be4ccdbc45469ef200fa1ea9f1c9146d0b50b437
Lasse Rafn <lasserafn@gmail.com>
2016-11-17 14:15:39 +0100

    Fixed config.. again.. fuck me..

--- src/CvrApiServiceProvider.php
+++ src/CvrApiServiceProvider.php
@@ -16,7 +16,17 @@ class CvrApiServiceProvider extends ServiceProvider
 	 */
 	public function boot()
 	{
-		$configPath = __DIR__ . '/../config/cvrapi.php';
+		$configPath = __DIR__ . '/config/cvrapi.php';
 		$this->mergeConfigFrom($configPath, 'cvrapi');
+
+		$configPath = __DIR__ . '/config/cvrapi.php';
+
+		if (function_exists('config_path')) {
+			$publishPath = config_path('cvrapi.php');
+		} else {
+			$publishPath = base_path('config/cvrapi.php');
+		}
+
+		$this->publishes([$configPath => $publishPath], 'config');
 	}
 }
