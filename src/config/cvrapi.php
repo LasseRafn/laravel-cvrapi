694f03464fec922917a8489035cbddc740c53dc1
Lasse R <lasserafn@gmail.com>
2022-01-18 16:35:03 +0000

    Update cvrapi.php

--- src/config/cvrapi.php
+++ src/config/cvrapi.php
@@ -1,5 +1,6 @@
 <?php
 return [
 	'endpoint'      => 'https://cvrapi.dk/api',
-	'your_business' => 'Your business name' // required, according to their API docs.
+	'your_business' => 'Your business name', // required, according to their API docs.
+	'token' 	=> '', // optional API token if you have a paid subscription
 ];
